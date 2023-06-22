ff01572d80b9d3927ed81620c5bae3a060fb8d77
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-06-22 11:16:04 +0200

    Created DTOs for Insult Model

---
+++ EvilInsultAPI/Models/DTOs/InsultDTOs/InsultPostDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EvilInsultAPI.Models.DTOs.InsultDTOs
+{
+    public class InsultPostDTO
+    {
+        public string InsultName { get; set; } = null!;
+        public string AuthorName { get; set; } = null!;
+        public string Language { get; set; } = null!;
+        public string InsultText { get; set; } = null!;
+    }
+}
