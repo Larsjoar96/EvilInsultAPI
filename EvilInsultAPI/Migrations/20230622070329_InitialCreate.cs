9fc5f8942d496d8cc1d5903d5c8f3afc8b28c3b0
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-06-22 09:40:09 +0200

    Testing database and ICrudService

---
+++ EvilInsultAPI/Migrations/20230622070329_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvilInsultAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Insults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InsultName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InsultText = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insults", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Insults");
+        }
+    }
+}
---
+++ EvilInsultAPI/Services/ICrudService.cs
@@ -0,0 +1,11 @@
+﻿namespace EvilInsultAPI.Services
+{
+    public interface ICrudService<T, ID>
+    {
+        Task<ICollection<T>> GetAllAsync();
+        Task<T> GetByIdAsync(ID id);
+        Task AddAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task DeleteByIdAsync(ID id);
+    }
+}
